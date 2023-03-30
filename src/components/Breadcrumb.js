066fac0fa22d786794378862001e1b2ad914ece2
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-03-30 20:22:23 +0700

    modify the breadcrumb

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,13 +1,37 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Breadcrumb({ links }) {
   const location = useLocation();
 
+  const [lastScrollPos, setLastScrollPos] = useState(0);
+  const [shouldShowHeader, setShouldShowHeader] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+
+      if (currentScrollPos > lastScrollPos && shouldShowHeader) {
+        setShouldShowHeader(false);
+      } else if (currentScrollPos < lastScrollPos || currentScrollPos === 0) {
+        setShouldShowHeader(true);
+      }
+
+      setLastScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [lastScrollPos, shouldShowHeader]);
+
   return (
     <nav
-      className="sticky top-[81px] border-b border-gray-200 bg-white"
-      style={{ marginTop: "81px" }}
+      // className="sticky top-[81px] z-10 border-b border-gray-200 bg-white"
+      className={`fixed left-0 right-0 top-0 z-10 border-b border-gray-200 bg-white transition-all duration-300 ${
+        shouldShowHeader ? '' : '-translate-y-full py-3'
+      }`}
+      style={{ marginTop: '81px' }}
     >
       <div className="container mx-auto max-w-screen-xl px-3">
         <ol className="flex items-center justify-center p-4 sm:justify-normal">
@@ -17,9 +41,9 @@ export default function Breadcrumb({ links }) {
                 to={link.path}
                 className={`${
                   location.pathname === link.path
-                    ? "text-blue-primary"
-                    : "text-gray-500 hover:text-blue-primary"
-                }`}
+                    ? 'text-blue-primary'
+                    : 'text-gray-500 hover:text-blue-primary'
+                } py-0`}
               >
                 {link.name}
               </Link>
