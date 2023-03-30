0cd372831ff31051147a9c9542ca4f1611e3670e
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-03-30 20:21:09 +0700

    add new path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ContactPage from "./pages/ContactPage";
-import GalleryPage from "./pages/GalleryPage";
-import LandingPage from "./pages/LandingPage";
-import NewsPage from "./pages/NewsPage";
-import ProfilePage from "./pages/ProfilePage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AgendaActivitiesPage from './pages/AgendaActivitiesPage';
+import ContactPage from './pages/ContactPage';
+import GalleryPage from './pages/GalleryPage';
+import LandingPage from './pages/LandingPage';
+import NewsPage from './pages/NewsPage';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
           <Route exact path="/" element={<LandingPage />} />
           <Route path="/profil-desa" element={<ProfilePage />} />
           <Route path="/galeri-desa" element={<GalleryPage />} />
-          <Route path="/berita-seputar-desa" element={<NewsPage />} />
+          <Route path="/berita-terbaru" element={<NewsPage />} />
+          <Route path="/agenda-kegiatan-desa" element={<AgendaActivitiesPage />} />
           <Route path="/kontak" element={<ContactPage />} />
         </Routes>
       </Router>
