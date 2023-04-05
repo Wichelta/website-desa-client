16ac859e5448fa5ce75fd9f6fbb4c6c7773ea73e
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-04-06 01:22:32 +0700

    fix bug can't scroll in carousel

--- src/components/Home/Carousel.js
+++ src/components/Home/Carousel.js
@@ -11,33 +11,29 @@ export default function CarouselImg() {
 
   return (
     <Fade direction="down" triggerOnce>
-      <div className="carousel-container">
-        <Carousel showThumbs={false} infiniteLoop={true} autoPlay={true}>
-          {images.map((image, index) => (
-            <div key={index}>
-              <div className="carousel-image-container">
-                <img
-                  src={image}
-                  alt={`Slide ${index}`}
-                  className="carousel-image h-screen select-none object-cover brightness-50"
-                />
-                <div className="carousel-text-container mt-5 flex w-full flex-col gap-5 md:max-w-5xl">
-                  <Fade direction="up" cascade damping={0.3}>
-                    <h2 className="text-4xl font-bold sm:text-5xl md:text-6xl">Selamat Datang</h2>
-                    <p className="max-w-md self-center text-lg sm:text-xl md:max-w-2xl md:text-2xl lg:max-w-full">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor velit at
-                      erat dictum mollis.
-                    </p>
-                    <button className="w-max select-none self-center rounded-md bg-blue-primary px-5 py-2 font-bold text-white transition duration-300 ease-in-out hover:bg-sky-950">
-                      Jelajahi
-                    </button>
-                  </Fade>
-                </div>
-              </div>
+      <Carousel swipeable={false} showThumbs={false} infiniteLoop={true} autoPlay={true}>
+        {images.map((image, index) => (
+          <div key={index}>
+            <img
+              src={image}
+              alt={`Slide ${index}`}
+              className="h-screen select-none object-cover brightness-50"
+            />
+            <div className="carousel-text-container mt-5 flex w-full flex-col items-center gap-5 md:max-w-5xl">
+              <Fade direction="up" cascade damping={0.3}>
+                <h2 className="text-4xl font-bold sm:text-5xl md:text-6xl">Selamat Datang</h2>
+                <p className="max-w-md self-center text-lg sm:text-xl md:max-w-2xl md:text-2xl lg:max-w-full">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor velit at erat
+                  dictum mollis.
+                </p>
+                <button className="w-max select-none self-center rounded-md bg-blue-primary px-5 py-2 font-bold text-white transition duration-300 ease-in-out hover:bg-sky-950">
+                  Jelajahi
+                </button>
+              </Fade>
             </div>
-          ))}
-        </Carousel>
-      </div>
+          </div>
+        ))}
+      </Carousel>
     </Fade>
   );
 }
