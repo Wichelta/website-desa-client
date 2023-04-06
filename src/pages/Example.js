048de9646913ca53852149dcbc8fe0c2e1932aaf
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-04-06 17:37:58 +0700

    create an example page to try develop new features

--- src/pages/Example.js
+++ src/pages/Example.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Fade } from 'react-awesome-reveal';
 
-export default function Example() {
-  return <div>Example</div>;
+function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsVisible(true);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click Me!</button>
+      {isVisible && (
+        <Fade onVisibilityChange={(visible) => setIsVisible(visible)}>
+          <div>
+            <h1>Awesome Reveal</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor eros vitae ipsum
+              aliquet eleifend. In hac habitasse platea dictumst. Quisque pulvinar erat et metus
+              congue, quis malesuada tortor venenatis.
+            </p>
+          </div>
+        </Fade>
+      )}
+    </div>
+  );
 }
+
+export default App;
