8a174a3ad59c59537770e8a27086504aaac05565
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-04-17 02:18:20 +0700

    create sample profile component

--- src/components/Profile/ProfileDetails.js
+++ src/components/Profile/ProfileDetails.js
@@ -1,42 +1,64 @@
 import React from 'react';
+import LogoDesa from '../../assets/images/logo_desa.png';
 
-export default function Profile({ data }) {
-  const image = data[0];
-
+export default function Profile() {
   return (
     <main className="mx-auto w-full bg-white">
-      <div className="container mx-auto mt-40 max-w-screen-xl px-4 py-5 sm:px-6 lg:px-4">
-        <div className="mb-6 max-w-screen-xl flex-col items-center justify-center px-3">
-          <h1 className="mb-5 text-center text-3xl font-bold text-gray-900">{image.title}</h1>
-          <img src={image.src} alt={image.alt} className="h-[600px] w-full object-cover" />
-        </div>
-        <div className="px-3 py-6">
-          <h2 className="text-lg font-medium text-gray-900">Sejarah Desa</h2>
-          <p className="mt-2 text-sm text-gray-500">
-            Desa ini didirikan pada tahun 1800-an oleh sekelompok petani yang mencari tempat yang
-            subur untuk bercocok tanam. Pada awalnya, desa ini hanya terdiri dari beberapa rumah
-            kecil yang dikelilingi oleh ladang-ladang jagung dan padi. Namun seiring waktu, desa ini
-            berkembang pesat dan menjadi salah satu pusat perdagangan terbesar di daerah tersebut.
-            Saat ini, desa ini memiliki lebih dari 10.000 penduduk dan menjadi tujuan wisata yang
-            populer karena keindahan alamnya.
-          </p>
-        </div>
-        <div className="px-3 py-6">
-          <h2 className="text-lg font-medium text-gray-900">Visi</h2>
-          <p className="mt-2 text-sm text-gray-500">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac consequat diam. Mauris
-            nec turpis nec lacus gravida vulputate. Sed nulla elit, volutpat quis sapien vel, tempor
-            tincidunt augue.
-          </p>
-        </div>
-        <div className="px-3 py-6">
-          <h2 className="text-lg font-medium text-gray-900">Misi</h2>
-          <ul className="mt-2 list-inside list-disc text-sm text-gray-500">
-            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-            <li>Sed ac consequat diam.</li>
-            <li>Mauris nec turpis nec lacus gravida vulputate.</li>
-            <li>Sed nulla elit, volutpat quis sapien vel, tempor tincidunt augue.</li>
-          </ul>
+      <div className="container mx-auto mt-[8.5rem] flex max-w-screen-xl flex-col px-4 py-4 sm:py-16 lg:px-10">
+        <div className="flex w-full flex-col items-center gap-5 lg:flex-row">
+          <div className="w-full py-4 lg:w-1/3">
+            <img src={LogoDesa} alt="Logo desa" className="m-auto w-72 select-none" />
+          </div>
+          <div className="flex w-full flex-col gap-4 py-4 lg:w-2/3">
+            <div className="flex flex-col gap-2">
+              <h2 className="text-center text-2xl font-bold text-gray-900 sm:text-3xl lg:text-left">
+                Sejarah Singkat Desa BRIliant
+              </h2>
+              <p className="text-base text-gray-900">
+                Desa ini didirikan pada tahun 1800-an oleh sekelompok petani yang mencari tempat
+                yang subur untuk bercocok tanam. Pada awalnya, desa ini hanya terdiri dari beberapa
+                rumah kecil yang dikelilingi oleh ladang-ladang jagung dan padi. Namun seiring
+                waktu, desa ini berkembang pesat dan menjadi salah satu pusat perdagangan terbesar
+                di daerah tersebut. Saat ini, desa ini memiliki lebih dari 10.000 penduduk dan
+                menjadi tujuan wisata yang populer karena keindahan alamnya.
+              </p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h2 className="text-center text-2xl font-bold text-gray-900 sm:text-3xl lg:text-left">
+                Visi & Misi
+              </h2>
+              <div>
+                <p className="text-base font-medium text-gray-900">Visi :</p>
+                <p className="text-base text-gray-900">
+                  Mewujudkan Desa yang Maju, Sejahtera, dan Berbudaya Menuju Indonesia yang Lebih
+                  Baik.
+                </p>
+                <p className="text-base font-medium text-gray-900">Misi :</p>
+                <ul className="list-inside list-disc">
+                  <li>
+                    Meningkatkan kesejahteraan masyarakat desa melalui pemberdayaan ekonomi
+                    masyarakat dan peningkatan kualitas sumber daya manusia.
+                  </li>
+                  <li>
+                    Meningkatkan kualitas infrastruktur desa yang berkelanjutan dan ramah
+                    lingkungan.
+                  </li>
+                  <li>
+                    Meningkatkan kualitas pelayanan publik di desa yang transparan dan partisipatif.
+                  </li>
+                  <li>Meningkatkan kualitas lingkungan dan kebersihan desa.</li>
+                  <li>
+                    Melestarikan nilai-nilai budaya dan tradisi lokal untuk menjaga identitas dan
+                    kearifan lokal.
+                  </li>
+                  <li>
+                    Meningkatkan kerja sama antar instansi, organisasi, dan masyarakat dalam upaya
+                    membangun desa yang maju dan sejahtera.
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
