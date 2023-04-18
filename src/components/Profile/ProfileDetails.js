bd7d87b293fa1f61227a1bc267c1455d101e0269
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-04-18 20:40:11 +0700

    get data from parent

--- src/components/Profile/ProfileDetails.js
+++ src/components/Profile/ProfileDetails.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { Fade } from 'react-awesome-reveal';
 import HTMLReactParser from 'html-react-parser';
-import data from '../../json/profileData.json';
 
-export default function Profile() {
+export default function Profile({ profileDataJson }) {
   return (
     <main className="mx-auto w-full bg-white">
       <div className="container mx-auto mt-[8.5rem] flex max-w-screen-xl flex-col px-4 py-4 sm:py-16 lg:px-10">
         <div className="flex w-full flex-col items-center gap-5 lg:flex-row">
           <div className="w-full py-4 lg:w-1/3">
             <Fade direction="left" triggerOnce>
-              <img
-                src={data.profileData[0].src}
-                alt="Logo desa"
-                className="m-auto w-72 select-none"
-              />
+              <img src={profileDataJson.src} alt="Logo desa" className="m-auto w-72 select-none" />
             </Fade>
           </div>
           <div className="flex w-full flex-col gap-4 py-4 lg:w-2/3">
             <div className="flex flex-col gap-2 text-gray-900">
               <Fade direction="right" triggerOnce>
-                {HTMLReactParser(data.profileData[0].description)}
+                <h2>{profileDataJson.title}</h2>
+                {HTMLReactParser(profileDataJson.description)}
               </Fade>
             </div>
           </div>
