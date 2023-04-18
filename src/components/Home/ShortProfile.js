dc0796dd6a37f5b33e8e13302b53a09eebdca8e3
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-04-18 20:40:06 +0700

    get data from parent

--- src/components/Home/ShortProfile.js
+++ src/components/Home/ShortProfile.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Fade } from 'react-awesome-reveal';
 import HTMLReactParser from 'html-react-parser';
-import data from '../../json/profileData.json';
 
-export default function ShortProfile({ refShortProfile }) {
+export default function ShortProfile({ refShortProfile, shortProfileDataJson }) {
   return (
     <main className="mx-auto w-full bg-white" ref={refShortProfile}>
       <div className="container mx-auto flex max-w-screen-xl flex-col gap-4 px-4 py-4 sm:py-16 lg:px-10">
@@ -17,7 +16,7 @@ export default function ShortProfile({ refShortProfile }) {
           <div className="w-full py-4 lg:w-1/3">
             <Fade direction="left" triggerOnce>
               <img
-                src={data.profileData[0].src}
+                src={shortProfileDataJson.src}
                 alt="Logo desa"
                 className="m-auto w-72 select-none"
               />
@@ -26,7 +25,8 @@ export default function ShortProfile({ refShortProfile }) {
           <div className="flex w-full flex-col gap-4 py-4 lg:w-2/3">
             <div className="flex flex-col gap-2 text-gray-900">
               <Fade direction="right" triggerOnce>
-                {HTMLReactParser(data.profileData[0].description)}
+                <h2 className="text-2xl sm:text-3xl">{shortProfileDataJson.title}</h2>
+                {HTMLReactParser(shortProfileDataJson.description)}
               </Fade>
             </div>
           </div>
