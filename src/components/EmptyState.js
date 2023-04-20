c7ba919b2ca3352ea8a9d967fc14d41c64ad8183
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-04-21 00:57:40 +0700

    use aos effect

--- src/components/EmptyState.js
+++ src/components/EmptyState.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ImgEmptyState from '../assets/images/img_empty_state.svg';
-import { Fade } from 'react-awesome-reveal';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function EmptyState({ textColor }) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
-    <div className="col-span-full my-32 flex flex-col items-center justify-center gap-5">
-      <Fade direction="up" cascade damping={0.3} triggerOnce>
-        <img src={ImgEmptyState} alt="No data" className="m-auto w-80 select-none" />
-        <p className={`${textColor}`}>Belum ada data untuk saat ini.</p>
-      </Fade>
+    <div
+      data-aos="zoom-in"
+      data-aos-duration="500"
+      className="col-span-full my-32 flex flex-col items-center justify-center gap-5"
+    >
+      <img src={ImgEmptyState} alt="No data" className="m-auto w-80 select-none" />
+      <p className={`${textColor}`}>Belum ada data untuk saat ini.</p>
     </div>
   );
 }
