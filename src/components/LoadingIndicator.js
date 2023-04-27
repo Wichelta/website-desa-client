db1ad03691508c1fd2073f5c930a6b253c9d39c0
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-04-27 21:37:21 +0700

    make height dynamic

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BarLoader } from 'react-spinners';
 
-export default function LoadingIndicator() {
+export default function LoadingIndicator({ height }) {
   return (
-    <div className="flex h-[30rem] flex-col items-center justify-center gap-2">
+    <div className={`flex ${height} flex-col items-center justify-center gap-2`}>
       <BarLoader height={5} width={200} color={'#9CA3AF'} />
       <p className="text-gray-500">Memuat data...</p>
     </div>
